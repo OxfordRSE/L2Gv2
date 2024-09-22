a6a078f5da9fa15d416ed7b546c285e8618f1d99
Martin Lotz <lotzma@gmail.com>
2024-09-22 20:02:52 +0100

    Add files via upload

---
+++ local2global/__init__.py
@@ -0,0 +1,10 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+from local2global.utils import AlignmentProblem, WeightedAlignmentProblem, SVDAlignmentProblem
+from local2global.patch import Patch
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = ''
