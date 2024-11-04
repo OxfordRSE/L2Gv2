5d139b39d13e7cf7193a4743078dd80e6eec1880
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2024-11-04 14:49:37 +0000

    Add sphinx-autobuild.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'l2gv2'
-copyright = '2024'
-author = ''
+project = "l2gv2"
+copyright = "2024"
+author = ""
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_markdown_builder"]
+extensions = ["sphinx_markdown_builder", "sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
