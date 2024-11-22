a2f18711b7730c697012f449c7dbe96485d081aa
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2024-11-22 15:26:00 +0000

    Add a module doctring.

--- l2gv2/network/__init__.py
+++ l2gv2/network/__init__.py
@@ -17,6 +17,7 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
+""" TODO: module docstring for network/__init__.py"""
 from .npgraph import NPGraph
 from .tgraph import TGraph
 from .utils import *  # TODO: this should be removed
