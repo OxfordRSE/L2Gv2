722fcc7fb102f9eac5eb5838f55bcc578ed72a7f
Martin <lotzma@gmail.com>
2025-02-27 10:30:11 +0000

    Restructured the repo

--- l2gv2/embedding/__init__.py
+++ l2gv2/embedding/__init__.py
@@ -1,4 +1,13 @@
 from .gae import GAE, VGAE, VGAE_loss, GAE_loss
 from .train import lr_grid_search, train
+from .embedding import patch_embeddings
 
-__all__ = ["GAE", "VGAE", "VGAE_loss", "GAE_loss", "lr_grid_search", "train"]
+__all__ = [
+    "GAE",
+    "VGAE",
+    "VGAE_loss",
+    "GAE_loss",
+    "lr_grid_search",
+    "train",
+    "patch_embeddings",
+]
