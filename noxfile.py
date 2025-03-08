d8053a191516e25406fb294372b56761fa17a46f
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-03-08 17:39:31 +0000

    Use nox instead of Makefile
    This will simplify testing on multiple Python versions

---
+++ noxfile.py
@@ -0,0 +1,39 @@
+import nox
+
+PYTHON_VERSIONS = "3.10"
+REPO = "https://github.com/OxfordRSE/L2Gv2/blob/main/"
+
+
+@nox.session
+def lint(session):
+    "Lint code using pylint and ruff"
+    session.install("pylint", "ruff")
+    session.run("pylint", "l2gv2/**/*.py")
+    session.run("pylint", "tests/**/*.py")
+    session.run("ruff", "check")
+    session.run("ruff", "format", "--check")
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def tests(session):
+    "Run tests"
+    session.run("pip", "install", ".[dev]")
+    session.run("pytest", "-n", "auto", "--cov")
+
+
+@nox.session(name="unused-code", default=False)
+def unused_code(session):
+    """Shows unused code warnings using vulture.
+
+    This test always passes, and is used to create GitHub issues
+    to report on unused code.
+    """
+    session.install("vulture")
+    out = session.run(
+        "vulture", "l2gv2", "tests", external=True, silent=True, success_codes=[0, 3]
+    )
+    for line in out.splitlines():
+        file, lineno, msg = line.split(":")
+        if "variable" in msg:
+            continue
+        print(f"- [ ] [{file}:{lineno}]({REPO}{file}#L{lineno}) {msg}")
